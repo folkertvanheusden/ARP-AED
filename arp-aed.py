09d9338032e28a4c3a57b41adea16464b75979a9
folkert van heusden <folkert@testium.soleus.nu>
2026-08-09 17:14:28 +0200

    cli parameters

--- arp-aed.py
+++ arp-aed.py
@@ -3,22 +3,56 @@
 # MAC / IP address usage, by folkert@vanheusden.com
 # MIT license
 
+import getopt
 import ipaddress
 import os
 import socket
 import sys
 
 
-device = sys.argv[1]
-user = sys.argv[2]
+def usage():
+    print('-d  device to listen on')
+    print('-u  UID to run as')
+    print('-x  specify zero or more MAC addresses to ignore')
+    print('-l  ignore link-local (IP6)')
 
 
 def str_to_mac(s):
     return b''.join([int(e, 16).to_bytes(1) for e in s.split(':')])
 
 
-ignore_list = set([str_to_mac(e) for e in sys.argv[3:]])
+ignore_list = set()
 ignore_list.add(str_to_mac('ff:ff:ff:ff:ff:ff'))  # ignore broadcast
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "hd:u:x:l", ["help", "device=", 'uid=', 'ignore=', 'no-link-local'])
+except getopt.GetoptError as err:
+    usage()
+    sys.exit(1)
+
+device = None
+uid = None
+link_local = True
+
+for o, a in opts:
+    if o in ("-h", "--help"):
+        usage()
+        sys.exit(0)
+    elif o in ("-d", "--device"):
+        device = a
+    elif o in ("-u", "--uid"):
+        uid = a
+    elif o in ("-x", "--ignore"):
+        ignore_list.add(str_to_mac(a))
+    elif o in ("-l", "--no-link-local"):
+        link_local = False
+    else:
+        assert False, "unhandled option"
+
+if device is None:
+    print('Please specify an Ethernet adapter to listen (-d)')
+    usage()
+    sys.exit(1)
 
 
 def mac_to_str(m):
@@ -36,7 +70,8 @@ def ip6_to_str(i):
 s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
 s.bind((device, 0))
 
-os.setuid(int(user))
+if not uid is None:
+    os.setuid(int(uid))
 
 macs = { }
 ip4s = { }
@@ -64,18 +99,15 @@ def remember(macs, mac_from, ips, ip_from):
 while True:
     raw_packet, addr = s.recvfrom(65535)
 
-    if raw_packet[12] == 0x08 and raw_packet[13] == 0x00:  # IP4
-        mac_from = raw_packet[6:12]
-        if mac_from in ignore_list:
-            continue
+    mac_from = raw_packet[6:12]
+    if mac_from in ignore_list:
+        continue
 
-        ip4_from = ip4_to_str(raw_packet[14 + 12: 14 + 12 + 4])
+    if raw_packet[12] == 0x08 and raw_packet[13] == 0x00:  # IP4
+        ip4_from = ip4_to_str(raw_packet[14 + 12: 14 + 12 + 4])  # TODO handle VLANs
         remember(macs, mac_from, ip4s, ip4_from)
 
     if raw_packet[12] == 0x86 and raw_packet[13] == 0xdd:  # IP6
-        mac_from = raw_packet[6:12]
-        if mac_from in ignore_list:
-            continue
-
-        ip6_from = ip6_to_str(raw_packet[14 + 8: 14 + 8 + 16])
-        remember(macs, mac_from, ip6s, ip6_from)
+        if link_local == True or (link_local == False and raw_packet[14 + 8] != 0xfe):
+            ip6_from = ip6_to_str(raw_packet[14 + 8: 14 + 8 + 16])  # TODO handle VLANs
+            remember(macs, mac_from, ip6s, ip6_from)
